1381584fd8f102fc992cbd4921e3f9a6efa3d9d3
Jason-Bloomer <86666099+Jason-Bloomer@users.noreply.github.com>
2022-07-15 00:28:59 -0700

    Add files via upload

---
+++ DUImageToLuaConverter/DUImageToLuaConverter/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(141, 220)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(132, 42)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Convert"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(13, 13)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(260, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Browse"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 42)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(260, 172)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(279, 13)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(346, 278)
+        Me.TextBox2.TabIndex = 8
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 268)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(261, 23)
+        Me.ProgressBar1.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 223)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Scale:"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(52, 221)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {20, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(83, 20)
+        Me.NumericUpDown1.TabIndex = 12
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {6, 0, 0, 0})
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 244)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Orient:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"None", "Rotate 90", "Rotate 180", "Rotate 270"})
+        Me.ComboBox1.Location = New System.Drawing.Point(52, 241)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(83, 21)
+        Me.ComboBox1.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(634, 304)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "DU Image to Lua String Converter"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ DUImageToLuaConverter/DUImageToLuaConverter/Form1.vb
@@ -0,0 +1,98 @@
+﻿Public Class Form1
+    Dim ImageArray(100, 100, 2) As Byte
+    Dim nativeWidth As Integer = 1024
+    Dim nativeHeight As Integer = 576
+    Dim resolutionScale As Integer = 8
+    Dim orientationMod As Integer = 0
+    Dim Rchan As Boolean = True
+    Dim Gchan As Boolean = True
+    Dim Bchan As Boolean = True
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ConvertImage(PictureBox1.Image)
+    End Sub
+
+    Private Sub ConvertImage(ByRef bm_combined As Bitmap)
+        ReDim ImageArray((nativeWidth / resolutionScale) - 1, (nativeHeight / resolutionScale) - 1, 2)
+        Dim bounds As Rectangle = New Rectangle(0, 0, bm_combined.Width, bm_combined.Height)
+        If bounds.Width > (nativeWidth / resolutionScale) - 1 Or bounds.Height > (nativeHeight / resolutionScale) - 1 Then
+            bm_combined = ResizeImage(bm_combined)
+        End If
+        Dim output As String = ""
+        For Y = 0 To bm_combined.Height - 1
+            For X = 0 To bm_combined.Width - 1
+                ImageArray(X, Y, 0) = bm_combined.GetPixel(X, Y).R / 2
+                ImageArray(X, Y, 1) = bm_combined.GetPixel(X, Y).G / 2
+                ImageArray(X, Y, 2) = bm_combined.GetPixel(X, Y).B / 2
+            Next X
+            Dim progress As Integer = ((Y / (bm_combined.Height - 1)) * 100)
+            ProgressBar1.Value = Progress
+        Next Y
+        For Ya = 0 To (nativeHeight / resolutionScale) - 1
+            For Xa = 0 To (nativeWidth / resolutionScale) - 1
+                For Za = 0 To 2
+                    ImageArray(Xa, Ya, Za) = ImageArray(Xa, Ya, Za) + 33
+                    If ImageArray(Xa, Ya, Za) = 34 Then
+                        ImageArray(Xa, Ya, Za) = 33
+                    End If
+                    If ImageArray(Xa, Ya, Za) = 92 Then
+                        ImageArray(Xa, Ya, Za) = 91
+                    End If
+                    If ImageArray(Xa, Ya, Za) > 126 Then
+                        ImageArray(Xa, Ya, Za) = 126
+                    End If
+                    'If ImageArray(Xa, Ya, Za) > 193 Then
+                    'ImageArray(Xa, Ya, Za) = ImageArray(Xa, Ya, Za) + 3
+                    'End If
+                    'If ImageArray(Xa, Ya, Za) > 197 Then
+                    'ImageArray(Xa, Ya, Za) = 197
+                    'End If
+                Next Za
+                output = output & System.Text.Encoding.ASCII.GetString({ImageArray(Xa, Ya, 0), ImageArray(Xa, Ya, 1), ImageArray(Xa, Ya, 2)})
+            Next Xa
+        Next Ya
+        TextBox2.Text = output
+    End Sub
+
+    Public Sub NewTextMsg(ByVal input As String)
+        TextBox2.Text = TextBox2.Text & input
+        TextBox2.ScrollToCaret()
+        Application.DoEvents()
+    End Sub
+
+    Public Function ResizeImage(ByVal InputImage As Image) As Image
+        Dim tempimg = InputImage
+        If orientationMod = 1 Then
+            tempimg.RotateFlip(RotateFlipType.Rotate90FlipNone)
+        End If
+        If orientationMod = 2 Then
+            tempimg.RotateFlip(RotateFlipType.Rotate180FlipNone)
+        End If
+        If orientationMod = 3 Then
+            tempimg.RotateFlip(RotateFlipType.Rotate270FlipNone)
+        End If
+        Dim tempbmp As Bitmap = New Bitmap(tempimg, New Size((nativeWidth / resolutionScale), (nativeHeight / resolutionScale)))
+        Return tempbmp
+    End Function
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            Try
+                Using fs As New System.IO.FileStream(OpenFileDialog1.FileName, IO.FileMode.Open)
+                    PictureBox1.Image = New Bitmap(Image.FromStream(fs))
+                End Using
+            Catch ex As Exception
+                MessageBox.Show("An error occurred while loading the file:" & Environment.NewLine & OpenFileDialog1.FileName & Environment.NewLine & Environment.NewLine & "Exception: " & ex.ToString & Environment.NewLine & ex.Message, "Error")
+            End Try
+        End If
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        resolutionScale = NumericUpDown1.Value
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        orientationMod = ComboBox1.SelectedIndex
+    End Sub
+End Class
